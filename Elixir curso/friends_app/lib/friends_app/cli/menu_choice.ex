71dd3745ca177c5fb92c426d0c9d0a5a5e26a368
Leonardo Gonçalves <leogonsil97@gmail.com>
2019-12-13 17:40:58 -0300

    Escolhendo um item do menu

--- Elixir curso/friends_app/lib/friends_app/cli/menu_choice.ex
+++ Elixir curso/friends_app/lib/friends_app/cli/menu_choice.ex
@@ -1,13 +1,21 @@
 defmodule FriendsApp.CLI.MenuChoice do
   alias Mix.Shell.IO, as: Shell
-
   def start do
     Shell.cmd("clear")
     Shell.info("Escolha uma opcao: ")
 
-    FriendsApp.CLI.MenuItens.all()
+    menu_itens = FriendsApp.CLI.MenuItens.all()
+
+    find_menu_item_by_index = &Enum.at(menu_itens, &1)
+
+    menu_itens
     |> Enum.map(&(&1.label))
     |> display_options()
+    |> generate_question()
+    |> Shell.prompt
+    |> parse_answer()
+    |> find_menu_item_by_index.()
+    |> confirm_menu_item()
 
   end
 
@@ -15,6 +23,27 @@ defmodule FriendsApp.CLI.MenuChoice do
     ops
     |> Enum.with_index(1)
     |> Enum.each(fn {op,index} -> Shell.info("#{index} - #{op}") end)
+    ops
   end
 
+  defp generate_question(ops) do
+    ops = Enum.join(1..Enum.count(ops),",")
+    "Qual das opções acima você escolhe? [#{ops}]\n"
+  end
+
+  defp parse_answer(answer) do
+    {option, _} = Integer.parse(answer)
+    option - 1
+  end
+
+  defp confirm_menu_item(item) do
+    Shell.cmd("clear")
+    Shell.info("Você escolheu... [#{item.label}]")
+
+    if Shell.yes?("Confirma?") do
+      Shell.info("...[#{item.label}]")
+    else
+      start()
+    end
+  end
 end
